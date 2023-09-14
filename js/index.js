1e75b983dd2a49a07459e1953dc4e779e3c855a8
Sergio Caballero <sergiocaballero1304@gmail.com>
2023-09-14 02:17:33 -0300

    avance de js

---
+++ js/index.js
@@ -0,0 +1,63 @@
+const notas = [];
+
+function agregarNota(listaNotas) {
+
+    let Tbl = document.getElementById("tablaNotas");
+
+    let hilera = document.createElement("tr");
+
+    let celdaId = document.createElement("td");
+    celdaId.appendChild(document.createTextNode(listaNotas[0]));
+    hilera.appendChild(celdaId);
+
+    for (let i = 1; i < listaNotas.length; i++) {
+
+        let celdaNota = document.createElement("td");
+        let textoCelda = document.createTextNode(listaNotas[i]);
+
+        celdaNota.appendChild(textoCelda);
+        hilera.appendChild(celdaNota);
+
+    }
+
+    let celdaPromedio = document.createElement("td");
+    let calcularPromedio = () => {
+        let sumaNotas = 0;
+        for (let i = 1; i < listaNotas.length; i++) {
+            sumaNotas += listaNotas[i];
+        }
+        return sumaNotas / listaNotas.length;
+    }
+
+    let promedio = calcularPromedio();
+    celdaPromedio.appendChild(document.createTextNode(promedio));
+    hilera.appendChild(celdaPromedio);
+
+    let celdaAprobado = document.createElement("td");
+    let aprobado = (promedio > 6) ? "Si" : "No";
+    celdaAprobado.appendChild(document.createTextNode(aprobado));
+    hilera.appendChild(celdaAprobado);
+    Tbl.appendChild(hilera);
+
+    let notaAlumno = { "id": listaNotas[0], "nota1": listaNotas[1], "nota2": listaNotas[2], "nota3": listaNotas[3] };
+    notas.push(notaAlumno);
+
+}
+
+function pedirNotas() {
+    let notas = [];
+    let id = prompt("Ingrese el ID del alumno:");
+    notas[0] = id;
+    for (let i = 1; i < 4; i++) {
+        let nota = parseInt(prompt(`Ingrese la nota número ${i}: (1-12)`));
+        while (nota < 1 || nota > 12) {
+            nota = parseInt(prompt("ENTRE 1 Y 12 TE DIJE LA PUTA QUE TE PARIO"));
+        }
+        notas[i] = nota;
+    }
+    agregarNota(notas);
+}
+
+
+
+const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
